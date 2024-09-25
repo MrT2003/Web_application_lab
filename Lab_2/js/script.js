4cde37526e64a692b81151d1d416a8336c692d2a
MrT2008 <haitrieu6212@gmail.com>
2024-09-25 23:15:15 +0700

    add blur header (not work)

---
+++ Lab_2/js/script.js
@@ -0,0 +1,8 @@
+/*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header")
+};
+window.addEventListener('scroll', blurHeader)
